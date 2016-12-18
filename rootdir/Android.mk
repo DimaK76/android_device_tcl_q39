2272c01e2622358cd9a441c3729decafa8e41f0a
DimaK76 <dimak76@mail.ru>
2016-12-18 21:45:41 +0300

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -223,3 +223,10 @@ LOCAL_SRC_FILES    := etc/init.qcom.usb.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := hosts
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/hosts
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
