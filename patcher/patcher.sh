b29075947df0a6df6dd869b0a3b71dc6d7ab9546
DimaK76 <3400107+DimaK76@users.noreply.github.com>
2017-11-25 21:34:39 +0300

    Add files via upload

--- patcher/patcher.sh
+++ patcher/patcher.sh
@@ -15,9 +15,15 @@ git -C system/core/liblog		reset -q --hard
 git -C system/core/rootdir		clean -dfqx
 git -C system/core/rootdir		reset -q --hard
 
+git -C frameworks/base/telephony/java/android/telephony/		clean -dfqx
+git -C frameworks/base/telephony/java/android/telephony/		reset -q --hard
+
 ### Patches
 git -C packages/apps/LockClock		apply ~/RR/device/tcl/q39/patcher/patches/packages-apps-LockClock0.patch
 git -C packages/apps/Settings/src/com/android/settings		apply ~/RR/device/tcl/q39/patcher/patches/DevelopmentSettings0.patch
 
 git -C system/core/liblog/		apply ~/RR/device/tcl/q39/patcher/patches/logprint0.patch
 git -C system/core/rootdir/		apply ~/RR/device/tcl/q39/patcher/patches/init0.patch
+
+git -C frameworks/base/telephony/java/android/telephony/		apply ~/RR/device/tcl/q39/patcher/patches/SignalStrength0.patch
+
