99919af65dd7a505efa1eaffb0d46d7c272f89f9
DimaK76 <3400107+DimaK76@users.noreply.github.com>
2018-06-08 23:27:25 +0300

    Update patcher.sh

--- patcher/patcher.sh
+++ patcher/patcher.sh
@@ -15,6 +15,9 @@ git -C system/core/liblog		reset -q --hard
 git -C system/core/rootdir		clean -dfqx
 git -C system/core/rootdir		reset -q --hard
 
+git -C build    clean -dfqx
+git -C build    reset -q --hard
+
 git -C frameworks/base/telephony/java/android/telephony/		clean -dfqx
 git -C frameworks/base/telephony/java/android/telephony/		reset -q --hard
 git -C frameworks/base/packages/SystemUI/src/com/android/systemui/qs/tiles/		clean -dfqx
@@ -28,6 +31,8 @@ git -C packages/apps/Settings/src/com/android/settings		apply ~/RR/device/tcl/q3
 
 git -C system/core/liblog/		apply ~/RR/device/tcl/q39/patcher/patches/logprint0.patch
 git -C system/core/rootdir/		apply ~/RR/device/tcl/q39/patcher/patches/init0.patch
+
+git -C build/core/clang/    apply ~/RR/device/tcl/q39/patcher/patches/CLANG0.patch
 
 git -C frameworks/base/telephony/java/android/telephony/		apply ~/RR/device/tcl/q39/patcher/patches/SignalStrength0.patch
 git -C frameworks/base/packages/SystemUI/src/com/android/systemui/qs/tiles/		apply ~/RR/device/tcl/q39/patcher/patches/MusicTile0.patch
